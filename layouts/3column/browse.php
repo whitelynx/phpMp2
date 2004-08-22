2022521a70591127aa395b17a496fcc44aa813e9
David H. Bronke <whitelynx@gmail.com>
2004-08-22 01:17:00 -0600

    Imported phpMp2 version 1.0.0-rc1

---
+++ layouts/3column/browse.php
@@ -0,0 +1,34 @@
+<body>
+
+<div id="topbar">
+<table class="nostyle">
+<tr>
+<td class="anotfull">Current&nbsp;Directory:&nbsp;<?php dir_links (array_key_exists("directory", $configuration) ? $configuration["directory"] : "") ?></td>
+<td style="width: 10%"><?php make_link("", "playlist", "Current&nbsp;Playlist", array(), false, null, "playlist") ?></td>
+<td style="width: 10%"><?php make_link("", "search", "Find&nbsp;Songs", array(), false, null, "search") ?></td>
+<td style="width: 10%"><?php make_link("", "files", "Files", array(), false, null, "search") ?></td>
+<td style="width: 10%"><?php make_link("", "directories", "Directories", array(), false, null, "search") ?></td>
+<td style="width: 10%"><?php make_link("", "playlists", "Available&nbsp;Playlists", array(), false, null, "playlists") ?></td>
+<td style="width: 10%"><?php make_link("", "tagfiles", "Browse&nbsp;by&nbsp;ID3", array(), false, null, "tagfiles") ?></td>
+</tr>
+</table>
+</div>
+
+<div style="float: left; width: 29%">
+<?php include("playlist.php"); ?>
+</div>
+
+<div style="float: right; width: 18%">
+<?php include("status.php"); ?>
+<?php include("control.php"); ?>
+<?php include("auth.php"); ?>
+<?php include("options.php"); ?>
+</div>
+
+<div style="float: left; width: 50%">
+<?php include("artists.php"); ?>
+<?php include("albums.php"); ?>
+<?php include("tagfiles.php"); ?>
+</div>
+
+</body>
